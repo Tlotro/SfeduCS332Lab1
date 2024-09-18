82da01ffc5f91fdfd617d0b94e29f5e8951b3342
monikauer <astkalich@mail.ru>
2024-09-18 14:50:10 +0300

    Add

--- Lab1/Task1/Properties/AssemblyInfo.cs
+++ Lab1/Task1/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанных со сборкой.
-[assembly: AssemblyTitle("Task1")]
+[assembly: AssemblyTitle("WindowsFormsApp2")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Task1")]
+[assembly: AssemblyProduct("WindowsFormsApp2")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("20ff0c8d-fcf1-4162-a64f-919f8251eb17")]
+[assembly: Guid("27b70004-5a1b-48ef-9252-b3a1fa62c6d7")]
 
 // Сведения о версии сборки состоят из указанных ниже четырех значений:
 //
